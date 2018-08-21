3bab7a39858bbb50b36b6eb85679b93cdf1269f5
Meghea Iulian <iulian.meghea@gmail.com>
2018-08-21 07:26:32 +0300

    [nixpkgs] remove ansible

--- .config/nixpkgs/overlays/ansible/default.nix
+++
@@ -1,7 +0,0 @@
-self: super: {
-  ansible_2_6 = with super; ansible_2_6.overrideAttrs(attrs: rec {
-    propagatedBuildInputs = attrs.propagatedBuildInputs ++ (with pkgs.python2.pkgs; [
-      jmespath
-    ]);
-  }); 
-}

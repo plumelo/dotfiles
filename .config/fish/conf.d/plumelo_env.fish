fed7615289746e086d5794b45e5ee77290d2e40f
Meghea Iulian <iulian.meghea@gmail.com>
2018-10-01 20:47:30 +0300

    [fish] fix LD_LIBRARY_PATH

--- .config/fish/conf.d/plumelo_env.fish
+++ .config/fish/conf.d/plumelo_env.fish
@@ -1,5 +1,5 @@
 set -gx PATH "$HOME/.npm-packages/bin" "$HOME/.local/bin" "$HOME/.gem/ruby/2.4.0/bin"  $PATH
 set -gx EDITOR nvim
 set -gx LAUNCHPAD_CHROME ~/.nix-profile/bin/google-chrome-stable
-set -g LD_LIBRARY_PATH "/run/opengl-driver/lib:/run/opengl-driver-32/lib"
+set -gx LD_LIBRARY_PATH "/run/opengl-driver/lib:/run/opengl-driver-32/lib"
 
